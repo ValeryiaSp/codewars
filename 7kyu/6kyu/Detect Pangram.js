8f3b91c8f159fb43672d30982d2e71f754bc0120
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-17 22:26:22 +0300

    add Detect Pangram task solution

--- 7kyu/6kyu/Detect Pangram.js
+++ 7kyu/6kyu/Detect Pangram.js
@@ -3,3 +3,16 @@ A pangram is a sentence that contains every single letter of the alphabet at lea
 
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 */
+
+function isPangram(string){
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i=0; i<string.length; i++) {
+    const char = string[i].toLowerCase();
+    if (characters.includes(char) && !result.includes(char)) {
+      result += char;
+    }
+  }
+  result = result.split('').sort().join('');
+  return result === characters;
+}
