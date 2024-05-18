d7db38c8b4586ca4eb7df846289d2bf2a0879c43
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-18 19:16:17 +0300

    add Let's Recycle task description

---
+++ 6kyu/Let's Recycle.js
@@ -0,0 +1,14 @@
+/* DESCROPTION: 
+
+Task
+You will be given a list of objects. Each object has type, material, and possibly secondMaterial. The existing materials are: paper, glass, organic, and plastic.
+
+Your job is to sort these objects across the 4 recycling bins according to their material (and secondMaterial if it's present), by listing the type's of objects that should go into those bins.
+
+Notes
+The bins should come in the same order as the materials listed above
+All bins should be listed in the output, even if some of them are empty
+If an object is made of two materials, its type should be listed in both of the respective bins
+The order of the type's in each bin should be the same as the order of their respective objects was in the input list
+*/
+
