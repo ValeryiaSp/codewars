31698f3a40a0b801999d03e720f099bf3d059ae2
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-19 11:29:13 +0300

    add Closures and Scopes task solution

--- 6kyu/Closures and Scopes.js
+++ 6kyu/Closures and Scopes.js
@@ -9,3 +9,12 @@ callbacks[3](); // must return 3
 We already implemented that function, but when we actually run the code, the result doesn't look like what we expected. Can you spot, what's wrong with it? A test fixture is also available
 
 */
+function createFunctions(n) {
+  let callbacks = [];
+  for (let i = 0; i < n; i++) {
+    callbacks.push(function() {
+      return i;
+    });
+  }
+  return callbacks;
+}
